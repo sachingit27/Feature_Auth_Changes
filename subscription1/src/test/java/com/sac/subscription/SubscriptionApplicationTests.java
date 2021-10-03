32f0c166bdc32145340b09fcccdf56808640c4c5
sachin <sachin@LAPTOP-1J9UUENO>
2021-10-03 16:47:31 -0700

    First Commit

---
+++ subscription1/src/test/java/com/sac/subscription/SubscriptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sac.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import junit.framework.Assert;
+
+@SpringBootTest
+class SubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.assertEquals(true, true);
+	}
+
+}
